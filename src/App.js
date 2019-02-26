eb564f053024f13d31f32ff5f38842f9eadd236d
Alex Puga <AlexPuga@AlexsMacBookPro.fios-router.home>
2019-02-26 16:56:42 -0500

    connected redux to react components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Provider } from "react-redux";
 
 import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
 
+import store from "./store";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Welcome.</h1>
-        </header>
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <h1>Welcome.</h1>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
